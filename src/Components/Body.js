6cbddae8e8a617e65d97c6d0fb479e1d13014110
Kaveri Chougule <chougulekaveri123@gmail.com>
2023-12-20 23:37:57 +0530

    Initial Commit

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -5,7 +5,7 @@ function Body(){
         <main>
             <div className="section1">
                 <div className="sec1Content">
-                    <h1 className='sec1Contentline1'>Let us find your <span className='forever'>Forever Food.</span></h1>
+                    <h1 className='sec1Contentline1'>Let us find your <br /><span className='forever'>Forever Food.</span></h1>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus numquam ea!</p>
                     <div className="sec1Btns">
                         <a href="#"><button className='searchNow'>Search Now</button></a>
